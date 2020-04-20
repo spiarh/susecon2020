82c5058431a63d8046d03ee0ad529aea4e60f804
lcavajani <lcavajani@suse.com>
2020-04-20 15:54:41 +0200

    update default password and clean charts
    Signed-off-by: lcavajani <lcavajani@suse.com>

--- misc/mariadb.sql
+++ misc/mariadb.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE prestashop;
-CREATE USER 'prestashop'@'%' IDENTIFIED BY 'prestashop';
+CREATE USER 'prestashop'@'%' IDENTIFIED BY 'susecon';
 GRANT ALL ON prestashop.* TO 'prestashop'@'%' WITH GRANT OPTION;
 CREATE DATABASE nextcloud;
-CREATE USER 'nextcloud'@'%' IDENTIFIED BY 'nextcloud';
+CREATE USER 'nextcloud'@'%' IDENTIFIED BY 'susecon';
 GRANT ALL ON nextcloud.* TO 'nextcloud'@'%' WITH GRANT OPTION;
